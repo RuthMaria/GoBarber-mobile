7d427ee705913336de88940394f4df35c9b949fb
RuthMaria <ruth-15rms@hotmail.com>
2021-04-03 01:08:42 -0300

    added unform

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,7 +3,6 @@ import { RectButton } from 'react-native-gesture-handler';
 
 // coloca entre parenteses para estilizar um componente que vem de fora do react native
 export const Container = styled(RectButton)`
-  width: 100%;
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 import { Container, TextInput, Icon } from './styles';
 
 // TextInputProps  tem todas as propriedades que um input pode ter
@@ -8,16 +9,47 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }: InputProps) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
+interface InputValueReference {
+  value: string;
+}
 
-    <TextInput
-      keyboardAppearance="dark"
-      placeholderTextColor="#666360"
-      {...rest}
-    />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }: InputProps) => {
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+  const inputElementRef = useRef<any>(null);
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+
+      <TextInput
+        ref={inputElementRef}
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
